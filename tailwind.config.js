cca704024a6a121cdd3154bbed000aeb0066a682
Siraaj <siraajisaacs001@gmail.com>
2024-04-04 22:06:28 +0200

    Final Footer Project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    colors: {
+      'teal': '#659f8f', 
+      'orange': '#d86943',
+      'gray': '#6b7280',
+      'white': '#f8fafc',
+      'black': '#000000',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
